da4730988c79e6919276ba995d827513a2d85442
Ameer Ahmad <ameer5ahmad@gmail.com>
2021-12-07 19:34:56 -0500

    setup server side for creating a todo

--- server/controllers/todo.js
+++ server/controllers/todo.js
@@ -1,3 +1,22 @@
-export const getTodos = (req, res) => {
-    res.send('Router is working');
+export const getTodos = async (req, res) => {
+    try {
+        const allTodos = await todo.find();
+
+        res.status(200).json(allTodos);
+    } catch (error) {
+        res.status(404).json({message: error.message});
+    }
+}
+
+export const createTodo = async (req, res) => {
+    const todo = req.body;
+
+    const newTodo = new todo(todo);
+
+    try {
+        await newTodo.save();
+        res.status(201).json(newTodo);
+    } catch (error) {
+        res.status(409).json({message: error.message});
+    }
 }
--- server/routes/todo.js
+++ server/routes/todo.js
@@ -1,8 +1,11 @@
 import express from "express";
-import { getTodos } from '../controllers/todo.js';
+import { getTodos, createTodo } from '../controllers/todo.js';
+import todo from '../models/todo.js';
 
 const router = express.Router();
 
 router.get('/', getTodos);
+router.post('/', createTodo);
+
 
 export default router;
