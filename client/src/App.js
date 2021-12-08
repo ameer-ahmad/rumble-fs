6f3fa5ea85ac9c5eae1dd34e6907b2ed959e246e
Ameer Ahmad <ameer5ahmad@gmail.com>
2021-12-07 19:47:55 -0500

    created react structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,35 @@
 import logo from './logo.svg';
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
+import Todo from './components/showTodo/showTodo.js'
+import Create from './components/createTodo/createTodo.js';
 import './App.css';
+import { mergeClasses } from '@material-ui/styles';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>SkillsBuild Training</h1>
-      </header>
+      <Container maxWidth="lg">
+        <AppBar className={classes.appBar} position="static" color="inherit">
+          <Typography className={classes.heading} variant="h2" align="center">Todos Create & Show</Typography>
+        </AppBar>
+
+        <Grow in>
+          <Container>
+            <Grid container justify="space-between" alignItems="stretch">
+              <Grid item xs={12} sm={7}>
+                <AppBar className={classes.appBar} position="static" color="inherit">
+                  <Todo />
+                </AppBar>
+              </Grid>
+              <Grid item xs={12} sm={4}>
+              <AppBar className={classes.appBar} position="static" color="inherit">
+                  <Create />
+                </AppBar>
+              </Grid>
+            </Grid>
+          </Container>
+        </Grow>
+      </Container>
     </div>
   );
 }
